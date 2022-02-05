e464fec954af81afa327d26d10b3db231ad7817d
jieun <23608596+jieunyoo@users.noreply.github.com>
2022-02-04 21:56:48 -0500

    add slider that updates based on input

--- catsVsDogs.py
+++ catsVsDogs.py
@@ -7,11 +7,11 @@ import altair as alt
 
 st.title('Cats vs Dogs - Popularity!')
 
-image = Image.open('pet.png')
+#image = Image.open('pet.png')
 
 st.text('Data from 49 states')
 
-st.image(image)
+#st.image(image)
 
 DATA_URL = ('https://raw.githubusercontent.com/jieunyoo/pets-stats/main/catsvdogs.csv')
 
@@ -28,9 +28,18 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
+#use slider values to populate data shown
+st.text('Adjust the slider to show percentage of dog owners between your selected values')
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (0.0, 50.0))
+#st.write('Values:', values)
 
-chart = alt.Chart(data).mark_bar().encode(x=alt.X('Location:N'), y=alt.Y('Percentage of Dog Owners:Q'))
-st.altair_chart(chart,use_container_width=True)
+
+filtered_data = data[data['Percentage of Dog Owners'].between(values[0], values[1], inclusive=True)]
+
+bottom = alt.Chart(filtered_data).mark_bar().encode(x=alt.X('Location:N'), y=alt.Y('Percentage of Dog Owners:Q'))
+st.altair_chart(bottom,use_container_width=True)
 
 
 catChart = alt.Chart(data).mark_bar().encode(x=alt.X('Location:N'), y=alt.Y('Percentage of Cat Owners:Q'))
